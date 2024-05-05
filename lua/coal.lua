d4336d28f90e2b3701f789b6c167638be321d776
swaroopanand <swaroopanand@email.com>
2024-05-05 23:44:12 +0530

    changed diff colors more

--- lua/coal.lua
+++ lua/coal.lua
@@ -268,12 +268,12 @@ function M.setup(config)
 		DiffChange = { bg = '#252a33', blend = 20 },
 		DiffDelete = { bg = '#3a273a', blend = 20 },
 		DiffText = { bg = '#394b70', blend = 20 },
-		diffAdded = { link = "DiffAdd" },
-		diffChanged = { link = "DiffChange" },
-		diffRemoved = { link = "DiffDelete" },
-		-- diffAdded = { fg = '#b8dbf7' },
-		-- diffChanged = { fg = "#7ca1f2" },
-		-- diffRemoved = { fg = "#e26a75" },
+		-- diffAdded = { link = "DiffAdd" },
+		-- diffChanged = { link = "DiffChange" },
+		-- diffRemoved = { link = "DiffDelete" },
+		diffAdded = { fg = '#b8dbf7' },
+		diffChanged = { fg = "#7ca1f2" },
+		diffRemoved = { fg = "#e26a75" },
 
 		-- Telescope
 		TelescopeSelectionCaret = { bg = palette.background.brightest },
